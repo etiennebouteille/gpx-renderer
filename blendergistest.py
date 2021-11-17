6612ff128332a585186c6ad5f9d203acdaed666f
Etienne <etienne.bouteille@gmail.com>
2021-11-17 23:00:43 +0000

    installed blender gis addon

---
+++ blendergistest.py
@@ -0,0 +1,2 @@
+import bpy
+bpy.ops.view3d.map_viewer('INVOKE_DEFAULT', srckey="GOOGLE", laykey="SAT", grdkey="WM", recenter=False)
--- installaddon.py
+++ installaddon.py
@@ -1,5 +1,8 @@
 #this is used to install the gpx importer addon on blender
+#use this in terminal :  blender -b --python-console
 import bpy
-bpy.ops.wm.addon_install(filepath="/home/pi/fourth-app/blender-gpx-master.zip")
-bpy.ops.wm.addon_enable(module="blender-gpx-master")
+bpy.ops.preferences.addon_install(filepath="/home/pi/gpx-renderer/BlenderGIS-225.zip")
+#bpy.ops.preferences.addon_install(filepath="/home/pi/gpx-renderer/blender-gpx-master.zip")
+#bpy.ops.preferences.addon_enable(module="blender-gpx-master")
+bpy.ops.preferences.addon_enable(module="BlenderGIS-225")
 bpy.ops.wm.save_userpref()
